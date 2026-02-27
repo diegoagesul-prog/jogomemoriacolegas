498cd7c44979a6b8ae408f6635266668481be893
diegoagesul-prog <diegoagesul@gmail.com>
2026-02-27 15:40:41 -0400

    pwa.js

---
+++ pwa.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registrado com sucesso:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Erro ao registrar Service Worker:', error);
+      });
+  });
+}
